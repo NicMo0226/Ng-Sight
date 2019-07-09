351b90127ee173d6c87cc08106dfc1fd8fe64214
Nicola Moorhouse <nic@NicsDebian>
2019-07-09 22:05:48 +0100

    chart styles

--- NgSight/src/app/charts/line-chart/line-chart.component.ts
+++ NgSight/src/app/charts/line-chart/line-chart.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { LINE_CHART_COLORS } from "../../shared/chart.colors";
+
+const LINE_CHART_SAMPLED_DATA: any[] = [
+  { data: [25, 12, 65, 97, 70, 41], label: "Product Analysis" },
+  { data: [35, 62, 87, 26, 60, 99], label: "Search Rankings" },
+  { data: [35, 11, 60, 23, 50, 89], label: "Budget Forecast" }
+];
+
+const LINE_CHART_SAMPLED_LABELS: string[] = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "April",
+  "May",
+  "June"
+];
 
 @Component({
-  selector: 'app-line-chart',
-  templateUrl: './line-chart.component.html',
-  styleUrls: ['./line-chart.component.css']
+  selector: "app-line-chart",
+  templateUrl: "./line-chart.component.html",
+  styleUrls: ["./line-chart.component.css"]
 })
 export class LineChartComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  lineChartData = LINE_CHART_SAMPLED_DATA;
+  lineChartLabels = LINE_CHART_SAMPLED_LABELS;
+  lineChartOptions: any = {
+    responsive: true
+  };
 
-  ngOnInit() {
-  }
+  lineChartLegend: true;
 
+  ngOnInit() {}
+
+  lineChartType = "line";
 }
---
+++ NgSight/src/app/shared/chart.colors.ts
@@ -0,0 +1,23 @@
+export const LINE_CHART_COLORS = [
+  {
+    backgroundColor: "rgba(6, 214, 160, 0.2)",
+    pointBackgroundColor: "#000",
+    borderColor: "rgba(0, 200, 140, 0.5)",
+    pointBorderColor: "#555",
+    pointHoverBorderColor: "#555"
+  },
+  {
+    backgroundColor: "rgba(255, 209 , 10, 0.2)",
+    pointBackgroundColor: "#000",
+    borderColor: "rgba(240, 180, 89, 0.5)",
+    pointBorderColor: "#555",
+    pointHoverBorderColor: "#555"
+  },
+  {
+    backgroundColor: "rgba(17, 85, 133, 0.2)",
+    pointBackgroundColor: "#000",
+    borderColor: "rgba(3, 62, 111, 0.5)",
+    pointBorderColor: "#555",
+    pointHoverBorderColor: "#555"
+  }
+];
